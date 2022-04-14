a6eede7667d0618849f37d73563f21494d74563c
valentin <valentinaless07@hotmail.com>
2022-04-14 19:27:45 -0300

    moviesInit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  const [movies, setMovies] = useState<Array<object>>([])
+
+  return (
+    <div className="App">
+   
+    </div>
+  )
+}
+
+export default App
