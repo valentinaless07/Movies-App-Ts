5375b6de562e9ddce0929410254c3b0fa38b91ca
valentin <valentinaless07@hotmail.com>
2022-04-18 21:29:57 -0300

    api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
 
   const [movies, setMovies] = useState<Array<object>>([])
+
+  useEffect( () => {
+    const getMovies = async () => {
+      const {data} = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_API_KEY}`)
+      setMovies(data.results)
+    }
+    getMovies()
+    
+  }, [])
+
+  
 
   return (
     <div className="App">
